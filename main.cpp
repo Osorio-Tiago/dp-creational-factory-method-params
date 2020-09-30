7d145c4a6d9eed369b623456e3b1228457a6a965
Maikol Guzman Alan <mguzmana@akamai.com>
2020-09-29 19:55:44 -0600

    New File
    Signed-off-by: Maikol Guzman Alan <mguzmana@akamai.com>

--- main.cpp
+++ main.cpp
@@ -2,16 +2,65 @@
 // Created by Maikol Guzman on 9/29/20.
 //
 #include <iostream>
+#include <utility>
+
+/**
+ * Enum with the type of element
+ */
+enum Buildings {
+    HOUSE, CASTLE, FARM
+};
+
+/**
+ * Base Spec Building
+ */
+struct BuildingSpec {
+    virtual Buildings buildingType() const = 0;
+};
+
+/**
+ * House Spec
+ */
+struct HouseSpec : public BuildingSpec {
+    Buildings buildingType() const override { return HOUSE; }
+
+    std::string houseName;
+
+    explicit HouseSpec(std::string houseName) : houseName(std::move(houseName)) {}
+};
+
+/**
+ * Farm Spec
+ */
+struct FarmSpec : public BuildingSpec {
+    Buildings buildingType() const { return FARM; }
+
+    bool withPasture;
+    int numberOfStalls;
+
+    FarmSpec(bool withPasture, int numberOfStalls) : withPasture(withPasture), numberOfStalls(numberOfStalls) {}
+};
+
+/**
+ * Castle Spec
+ */
+struct CastleSpec : public BuildingSpec {
+    Buildings buildingType() const { return CASTLE; }
+
+    int size;
+
+    explicit CastleSpec(int size) : size(size) {}
+};
 
 /**
  * Building
  * Base class to construct buildings
  */
-class Building
-{
+class Building {
 public:
     // Factory Method
-    static Building *MakeBuilding(int choice);
+    static Building *MakeBuilding(const BuildingSpec *buildingSpec);
+
     virtual std::string processMethod() = 0;
 };
 
@@ -19,12 +68,14 @@ public:
  * Derive class for House
  * Inherited from Building
  */
-class House: public Building {
+class House : public Building {
 private:
-    int numberDoors;
+    std::string houseName;
 public:
+    explicit House(const HouseSpec *houseSpec) : houseName(houseSpec->houseName) {}
+
     std::string processMethod() override {
-        return "Create a beautiful House";
+        return "Create a beautiful House NAME: " + houseName;
     }
 };
 
@@ -32,10 +83,15 @@ public:
  * Derive class for Castle
  * Inherited from Building
  */
-class Castle: public Building {
+class Castle : public Building {
+private:
+    int size;
+
 public:
+    explicit Castle(const CastleSpec *castleSpec) : size(castleSpec->size) {}
+
     std::string processMethod() override {
-        return "Create a huge Castle";
+        return "Create a huge Castle SIZE: " + std::to_string(size);
     }
 };
 
@@ -43,10 +99,20 @@ public:
  * Derive class for Farm
  * Inherited from Building
  */
-class Farm: public Building {
+class Farm : public Building {
+private:
+    bool withPasture;
+    int numberOfStalls;
+
 public:
+    explicit Farm(const FarmSpec *farmSpec) {
+        withPasture = farmSpec->withPasture;
+        numberOfStalls = farmSpec->numberOfStalls;
+    }
+
     std::string processMethod() override {
-        return "Create a amazing Farm";
+        return "Create a amazing Farm WITH PASTURE: " + std::to_string(withPasture) +
+               " NUMBER OF STALLS: " + std::to_string(numberOfStalls);
     }
 };
 
@@ -55,13 +121,15 @@ public:
  * @param choice of build
  * @return the corresponding instance
  */
-Building *Building::MakeBuilding(int choice) {
-    if (choice == 1)
-        return new House;
-    else if (choice == 2)
-        return new Castle;
-    else
-        return new Farm;
+Building *Building::MakeBuilding(const BuildingSpec *buildingSpec) {
+    switch (buildingSpec->buildingType()) {
+        case HOUSE:
+            return new House(dynamic_cast<const HouseSpec *>(buildingSpec));
+        case CASTLE:
+            return new Castle(dynamic_cast<const CastleSpec *>(buildingSpec));
+        case FARM:
+            return new Farm(dynamic_cast<const FarmSpec *>(buildingSpec));
+    }
 }
 
 /**
@@ -71,9 +139,13 @@ Building *Building::MakeBuilding(int choice) {
 int main() {
     std::cout << "Hello, Universidad Nacional!" << std::endl;
 
-    Building* newHouse = Building::MakeBuilding(1);
-    Building* newCastle = Building::MakeBuilding(2);
-    Building* newFarm = Building::MakeBuilding(3);
+    BuildingSpec *houseSpec = new HouseSpec("Super House");
+    BuildingSpec *castleSpec = new CastleSpec(100);
+    BuildingSpec *farmSpec = new FarmSpec(true, 5);
+
+    Building *newHouse = Building::MakeBuilding(houseSpec);
+    Building *newCastle = Building::MakeBuilding(castleSpec);
+    Building *newFarm = Building::MakeBuilding(farmSpec);
 
     std::cout << newHouse->processMethod() << std::endl;
     std::cout << newCastle->processMethod() << std::endl;
